786394cb292424297f61376a6ca1087c609b2e2c
Ben Fino-Radin <bfinoradin@gmail.com>
2015-12-30 12:17:12 -0500

    nav bar more or less done

--- js/nav.js
+++ js/nav.js
@@ -2,15 +2,28 @@
 
 
 $( document ).ready(function() {
-$('#button').click(function(){
-	if ($('#nav').height() < 166) {
+
+
+$('#menubutton').click(function(){
+	if ($('#nav').height() < 100) {
     $('#nav').animate({height:'166px'}, 300);
     $(this).html("Close");
+    $('#nav-pages').animate({opacity:'1'},500);
 } else {
 	$('#nav').animate({height:'64px'}, 300);
+	$('#nav-pages').animate({opacity:'0'},500);
 	$(this).html("Menu");
 }
 
     
 });
+
+
+    $(window).scroll(function(){
+           	if ($('#nav').height() > 100) {
+    $('#nav').animate({height:'64px'}, 400);
+    $('#menubutton').html("Menu");
+}    
+            });
+
 });
