bedfe0bb708e2802f1d00ee8c09c05dac89e47c1
Ben Fino-Radin <bfinoradin@gmail.com>
2016-01-13 16:42:57 -0500

    Revert "Revert "some re-org""
    This reverts commit 5563936590a734233ac5e9081545cd8fc707cffb.

--- js/nav.js
+++ js/nav.js
@@ -21,14 +21,14 @@ $( document ).ready(function() {
 // })
 
 // initial check in case doc is loading already scrolled down the page...
-        if ($(document).scrollTop() > 140){
+        if ($(document).scrollTop() > 120){
             $('#nav').stop().animate({height:'64px'}, 0);
             $('#nav-pages').stop().animate({opacity:'0'},0);
             $('#menubutton').stop().animate({opacity:'1'},0);
             $('#menubutton').html("Menu");
         }
-        else if ($(document).scrollTop() < 145){
-            $('#nav').stop().animate({height:'166px'}, 0);
+        else if ($(document).scrollTop() < 125){
+            $('#nav').stop().animate({height:'136px'}, 0);
             $('#nav-pages').stop().animate({opacity:'1'},0);
             $('#menubutton').stop().animate({opacity:'0'},0);
         }
@@ -36,7 +36,7 @@ $( document ).ready(function() {
 // when the menu / close button is clicked
     $('#menubutton').click(function(){
     	if ($('#nav').height() < 100) {
-            $('#nav').stop().animate({height:'166px'}, 300);
+            $('#nav').stop().animate({height:'136px'}, 300);
             $(this).html("Close");
             $('#nav-pages').stop().animate({opacity:'1'},500);
             $('.contextualmenu').stop().animate({opacity:'0'},300);
@@ -52,15 +52,15 @@ $( document ).ready(function() {
 // when the user scrolls
     $(window).scroll(function(){
 
-        if ($(document).scrollTop() > 140){
+        if ($(document).scrollTop() > 120){
             $('#nav').stop().animate({height:'64px'}, 200);
             $('#nav-pages').stop().animate({opacity:'0'},300);
             $('#menubutton').stop().animate({opacity:'1'},300);
             $('.contextualmenu').stop().animate({opacity:'1'},300);
             $('#menubutton').html("Menu");
         }
-        else if ($(document).scrollTop() < 145){
-            $('#nav').stop().animate({height:'166px'}, 100);
+        else if ($(document).scrollTop() < 125){
+            $('#nav').stop().animate({height:'136px'}, 100);
             $('#nav-pages').stop().animate({opacity:'1'},200);
             $('#menubutton').stop().animate({opacity:'0'},200);
             $('.contextualmenu').stop().animate({opacity:'0'},300);
