47f198aacc06d2e057907d9bb2bf0121d2e19197
Ben Fino-Radin <bfinoradin@gmail.com>
2016-03-08 16:12:07 -0500

    stuff and things

--- js/nav.js
+++ js/nav.js
@@ -68,11 +68,11 @@ else if ($(window).width() > 600) {
             $('#backtotop').stop().animate({opacity:'0'},150);
             $('.contextualmenu').stop().animate({top:'144px'},150);
         }
-    else if ($(document).scrollTop() > 120 && $(window).width() < 600){
-            $('#nav').stop().animate({height:navHeightCollapsed}, 200);
-            $('#nav-pages').stop().animate({opacity:'0'},150);
-            $('#about-faq').stop().animate({opacity:'0'},50);
-            $('.contextualmenu').stop().animate({top:'44px'},150);
+        else if ($(document).scrollTop() > 120 && $(window).width() < 600){
+                $('#nav').stop().animate({height:navHeightCollapsed}, 200);
+                $('#nav-pages').stop().animate({opacity:'0'},150);
+                $('#about-faq').stop().animate({opacity:'0'},50);
+                $('.contextualmenu').stop().animate({top:'44px'},150);
 
         }
         // else if ($(document).scrollTop() < 125 && $(window).width() < 600){
